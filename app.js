f50e01961a9c6af78fcddd966930080432697ddc
vatsal555 <prajapativatsal555@gmail.com>
2025-03-28 12:13:14 +0530

    implement swagger

--- app.js
+++ app.js
@@ -8,19 +8,20 @@ import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 import workflowRouter from "./router/workflow.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 // app.use(arcjetMiddleware)
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/workflows", workflowRouter);
-
 
 app.use(errorMiddleware);
 
